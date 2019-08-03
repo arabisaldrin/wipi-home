eda69f716dc68a76ce8fac2f32d23816ddde7aa0
Aldrin Arabis <arabis.aldrin@yandex.com>
2019-08-03 22:32:38 +0800

    Add result view in voucher testing

--- resources/pages/voucher-test/index.vue
+++ resources/pages/voucher-test/index.vue
@@ -1,7 +1,7 @@
 <template>
   <page title="Voucher Test">
     <v-container grid-list-md>
-      <v-card>
+      <v-card class="mb-2">
         <v-card-text>
           <v-layout justify-center column>
             <v-flex>
@@ -11,6 +11,11 @@
               <v-btn block color="primary" @click="submit">Submit</v-btn>
             </v-flex>
           </v-layout>
+        </v-card-text>
+      </v-card>
+      <v-card>
+        <v-card-text>
+          <pre>{{JSON.stringify(result, null, 2)}}</pre>
         </v-card-text>
       </v-card>
     </v-container>
@@ -23,13 +28,14 @@ export default {
     return {
       formData: {
         mac: "34-41-5D-D9-E3-06"
-      }
+      },
+      result: ""
     };
   },
   methods: {
     async submit() {
       const { data } = await axios.post("/portal/voucher", this.formData);
-      console.log(data);
+      this.result = data;
     }
   }
 };
