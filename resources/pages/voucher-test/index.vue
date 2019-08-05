7480e91e66730abbc83ba6456df97770c94f268f
Aldrin Arabis <arabis.aldrin@yandex.com>
2019-08-05 23:09:13 +0800

    Update voucher test page
    Add full schema request
    Add require validation

--- resources/pages/voucher-test/index.vue
+++ resources/pages/voucher-test/index.vue
@@ -5,7 +5,14 @@
         <v-card-text>
           <v-layout justify-center column>
             <v-flex>
-              <v-text-field v-model="formData.voucher" label="Voucher" @keypress.enter="submit"></v-text-field>
+              <v-text-field
+                v-model="formData.voucher"
+                v-validate="'required'"
+                :error="errors.has('Voucher')"
+                label="Voucher"
+                name="Voucher"
+                @keypress.enter="submit"
+              ></v-text-field>
             </v-flex>
             <v-flex>
               <v-btn block color="primary" @click="submit">Submit</v-btn>
@@ -27,15 +34,29 @@ export default {
   data() {
     return {
       formData: {
-        mac: "34-41-5D-D9-E3-06"
+        res: "notyet",
+        uamip: "192.168.10.1",
+        uamport: "3990",
+        challenge: "2c8aa29f59591f39a7c6a3c7d0f2dcf2",
+        called: "B8-27-EB-33-17-3D",
+        mac: "34-41-5D-D9-E3-06",
+        ip: "192.168.10.4",
+        ssid: "Arabis-GOWIFI",
+        nasid: "Kupiki",
+        sessionid: "156501567000000001",
+        userurl: "http://google.com/",
+        voucher: ""
       },
       result: ""
     };
   },
   methods: {
     async submit() {
-      const { data } = await axios.post("/portal/voucher", this.formData);
-      this.result = data;
+      const valid = await this.$validator.validateAll();
+      if (valid) {
+        const { data } = await axios.post("/portal/voucher", this.formData);
+        this.result = data;
+      }
     }
   }
 };
