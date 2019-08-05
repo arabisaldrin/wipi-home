6a288abd5856da1cf49c65f874de4602f4ff5345
Aldrin Arabis <arabis.aldrin@yandex.com>
2019-08-05 23:09:42 +0800

    Update portal controller
    Add Simultaneous-Use of the voucher plan

--- app/Http/Controllers/PortalController.php
+++ app/Http/Controllers/PortalController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Device;
 use App\Radcheck;
+use App\Radgroupcheck;
 use App\Radusergroup;
 use App\User;
 use App\Voucher;
+use App\VoucherLog;
 use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
@@ -23,33 +25,62 @@ class PortalController extends Controller
         $voucher = Voucher::active()->where('code', $request->voucher)->first();
 
         if ($voucher) {
+            $username = $request->mac . "_$voucher->code";
+            // get voucher plan
+            $plan = $voucher->plan()->first();
+
+            // if voucher already used
             if ($voucher->used_at) {
-                // get generated password
-                $radcheck = Radcheck::where('username', $request->mac)
+                // get logs for simultaneous checking
+                $check = Radgroupcheck::where('groupname', $plan->code)
+                    ->where('attribute', 'Simultaneous-Use')
+                    ->first();
+
+                // if [Simultaneous-Use] attribute exists check voucher count usage
+                // group by mac_address to specify actual devices count
+                if ($check) {
+                    $logCount = VoucherLog::where('voucher_code', $voucher->code)
+                        ->groupBy('mac_address')->count();
+                    if ($logCount == $check->value) {
+                        return response()->json([
+                            'status' => 0,
+                            'message' => 'Maximum device usage reach',
+                        ]);
+
+                    }
+                }
+                // passed [Simultaneous-Use] checking and can be used again
+                // get previous generated password
+                $radcheck = Radcheck::where('username', $username)
                     ->where('attribute', 'Cleartext-Password')
                     ->first();
 
             } else {
                 // if not used create voucher connection details
 
-                // get voucher plan
-                $plan = $voucher->plan()->first();
-
                 $radcheck = Radcheck::create([
-                    'username' => $request->mac,
+                    'username' => $username,
                     'attribute' => 'Cleartext-Password',
                     'op' => ':=',
                     'value' => Str::random(20),
                 ]);
                 // Add this voucher to radusergroup
                 Radusergroup::create([
-                    'username' => $request->mac,
+                    'username' => $username,
                     'groupname' => $plan->code,
                 ]);
                 // update voucher used_at timestamp
                 $voucher->used_at = Carbon::now();
                 $voucher->save();
+
             }
+            // log the voucher usage
+            VoucherLog::create([
+                'voucher_code' => $voucher->code,
+                'mac_address' => $request->mac,
+                'ssid' => $request->ssid,
+                'nasid' => $request->nasid,
+            ]);
 
             return response()->json([
                 'status' => 1,
