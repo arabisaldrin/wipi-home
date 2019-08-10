0214795024bae5c0f76c975cc6af96a40f2c1d68
Aldrin Arabis <arabis.aldrin@yandex.com>
2019-08-10 23:57:38 +0800

    Update dashboard

--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -11,59 +11,19 @@
           <v-layout row wrap>
             <v-flex sm6 xs12 md6 lg3>
               <stats-card color="green" icon="mdi-account-group" title="Users">
-                <template
-                  >{{ userCount.active || 0 }}/{{
-                    userCount.total
+                <template>
+                  {{ userCount.active || 0 }}/{{
+                  userCount.total
                   }}
-                  Active</template
-                >
-              </stats-card>
-            </v-flex>
-            <v-flex sm6 xs12 md6 lg3>
-              <stats-card
-                color="pink"
-                icon="mdi-chart-donut"
-                title="Download Usage"
-                sub-icon="mdi-update"
-                sub-text="Last 2 days"
-                sub-text-color="text-primary"
-              >
-                <template slot="icon">
-                  <div
-                    style="height: 100%; position: absolute; width: 100%; justify-content: center; display: flex;"
-                  >
-                    <v-icon small class="absolute">mdi-download</v-icon>
-                  </div>
-                  <v-icon dark size="40" class="ma-3">mdi-chart-donut</v-icon>
-                </template>
-                <template>
-                  <v-icon small>mdi-arrow-up</v-icon>
-                  {{ dataUsage.download | fix2 }} GB
+                  Active
                 </template>
               </stats-card>
             </v-flex>
             <v-flex sm6 xs12 md6 lg3>
-              <stats-card
-                color="orange"
-                icon="mdi-chart-donut"
-                title="Upload Usage"
-                sub-icon="mdi-update"
-                sub-text="Last 2 days"
-                sub-text-color="text-primary"
-              >
-                <template slot="icon">
-                  <div
-                    style="height: 100%; position: absolute; width: 100%; justify-content: center; display: flex;"
-                  >
-                    <v-icon small class="absolute">mdi-upload</v-icon>
-                  </div>
-                  <v-icon dark size="40" class="ma-3">mdi-chart-donut</v-icon>
-                </template>
-                <template>
-                  <v-icon small>mdi-arrow-down</v-icon>
-                  {{ dataUsage.upload | fix2 }} GB
-                </template>
-              </stats-card>
+              <download-usage />
+            </v-flex>
+            <v-flex sm6 xs12 md6 lg3>
+              <upload-usage />
             </v-flex>
             <v-flex sm6 xs12 md6 lg3>
               <stats-card
@@ -108,46 +68,31 @@
 import StatsCard from "@/components/dashboard/StatsCard";
 import MonthlyTrends from "@/components/dashboard/MonthlyTrends";
 import AnnualTrends from "@/components/dashboard/AnnualTrends";
+import DownloadUsage from "@/components/dashboard/DownloadUsage";
+import UploadUsage from "@/components/dashboard/UploadUsage";
 
 export default {
-  filters: {
-    fix2(val) {
-      return Number(val).toFixed(2);
-    }
-  },
   components: {
     StatsCard,
     MonthlyTrends,
-    AnnualTrends
+    AnnualTrends,
+    DownloadUsage,
+    UploadUsage
   },
   data() {
     return {
       userCount: {},
-      voucherCount: {},
-      dataUsage: {
-        upload: 0,
-        download: 0
-      },
-      annualTrends: {
-        labels: [],
-        datasets: []
-      }
+      voucherCount: {}
     };
   },
   async created() {
-    const [
-      { data: userCount },
-      { data: voucherCount },
-      { data: dataUsage }
-    ] = await Promise.all([
+    const [{ data: userCount }, { data: voucherCount }] = await Promise.all([
       axios.get("/dashboard/user-count"),
-      axios.get("/dashboard/voucher-count"),
-      axios.get("/dashboard/data-usage")
+      axios.get("/dashboard/voucher-count")
     ]);
 
     this.userCount = userCount;
     this.voucherCount = voucherCount;
-    this.dataUsage = dataUsage;
   }
 };
 </script>
