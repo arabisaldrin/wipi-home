fe9af8114f0bae7660473b412b40e9937b6fc994
Aldrin Arabis <arabis.aldrin@yandex.com>
2019-08-19 00:33:14 +0800

    Update i18n plugin
    Add auto import languages

--- resources/i18n/index.js
+++ resources/i18n/index.js
@@ -3,11 +3,32 @@ import Vue from 'vue';
 
 Vue.use(VueI18n);
 
+const messages = {};
+
+const requireComponent = require.context(
+	// The relative path of the components folder
+	'./lang',
+	// Whether or not to look in subfolders
+	false,
+	// The regular expression used to match base component filenames
+	/\w+\.(js)$/
+);
+
+requireComponent.keys().forEach(fileName => {
+	// Get component config
+	const langConfig = requireComponent(fileName);
+
+	const lang = fileName
+		.split('/')
+		.pop()
+		.replace(/\.\w+$/, '');
+
+	messages[lang] = langConfig.default || langConfig;
+});
+
 const i18n = new VueI18n({
 	locale: 'en-US',
-	messages: {
-		'en-US': {}
-	}
+	messages
 });
 
 export default i18n;
---
+++ resources/i18n/lang/en-US.js
@@ -0,0 +1,8 @@
+export default {
+	group: {
+		remove: {
+			success: 'Group has been successfully removed',
+			failed: 'err {status} : Some error occurred while trying to remove the voucher group'
+		}
+	}
+};
