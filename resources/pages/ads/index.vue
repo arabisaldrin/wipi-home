edba0503a7559bb52f17a945aac57031e2305e93
Aldrin Arabis <arabis.aldrin@yandex.com>
2019-08-19 23:41:46 +0800

    Ads index template

--- resources/pages/ads/index.vue
+++ resources/pages/ads/index.vue
@@ -1,18 +1,16 @@
 <template>
   <page title="Advertisements" scheme="blue-grey">
     <template slot="tools">
-      <v-btn fab color="primary" to="add" append>
+      <v-btn small fab color="primary" to="add" append>
         <v-icon>mdi-plus</v-icon>
       </v-btn>
-      <v-btn fab dark color="green" class="ml-2">
+      <v-btn small fab dark color="green" class="ml-2">
         <v-icon>mdi-printer</v-icon>
       </v-btn>
     </template>
-    <v-layout column>
-      <v-card>
-        <v-data-table :headers="headers" :items="[]" :options.sync="options" item-key="id"></v-data-table>
-      </v-card>
-    </v-layout>
+    <v-container grid-list-md>
+      <v-data-table :headers="headers" :items="[]" :options.sync="options" item-key="id"></v-data-table>
+    </v-container>
     <router-view></router-view>
   </page>
 </template>
@@ -21,7 +19,24 @@
 export default {
   data() {
     return {
-      headers: [],
+      headers: [
+        {
+          text: "Title",
+          value: "title"
+        },
+        {
+          text: "Description",
+          value: "description"
+        },
+        {
+          text: "Type",
+          value: "type"
+        },
+        {
+          text: "Date",
+          value: "date"
+        }
+      ],
       options: {}
     };
   }
